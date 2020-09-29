db808d77caa8eda91713a5162b3e256c618aef0e
mohitpanwar09 <sandypanwar09@gmail.com>
2020-09-29 15:24:16 +0530

    First commit Added terrain and player script

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float mouseSpeedy=2.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Mouse_y=mouseSpeedy*Input.GetAxis("Mouse Y");
+        if(Mouse_y<=90f){
+            transform.Rotate(-Mouse_y,0,0);
+        }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float _speed=8f;
+    public float _mouseSpeed=2.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput=Input.GetAxis("Horizontal")*_speed*Time.deltaTime;
+        float VirticalInput=Input.GetAxis("Vertical")*_speed*Time.deltaTime;
+
+            transform.Translate(0,0,VirticalInput);
+            transform.Translate(horizontalInput,0,0);
+            transform.Rotate(0f,0f,0f,Space.World);
+
+        float Mouse_x=_mouseSpeed*Input.GetAxis("Mouse X");
+        transform.Rotate(0,Mouse_x,0);
+    }
+}
